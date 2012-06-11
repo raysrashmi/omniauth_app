8c7e06b6e72ec07679491664562b066d93b3a7b6
Rashmi Yadav <rashmi14yadav@gmail.com>
2012-06-11 13:45:35 +0530

    correct file name

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 require 'openid/store/filesystem'
 Rails.application.config.middleware.use OmniAuth::Builder do
-  PROVIDER = YAML.load_file("#{RAILS_ROOT}/config/app_constants.yml")[RAILS_ENV]
+  PROVIDER = YAML.load_file("#{RAILS_ROOT}/config/provider.yml")[RAILS_ENV]
   
   provider :twitter, PROVIDER['twitter']['key'], PROVIDER['twitter']['secret']
   provider :facebook,  PROVIDER['facebook']['key'], PROVIDER['facebook']['secret']
